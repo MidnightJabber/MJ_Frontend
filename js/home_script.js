57d9d8728d23e062e5c65d1ab4323f43cc21c49d
Vishrut Reddi <vishrutreddi@gmail.com>
2015-02-03 15:22:05 -0600

    Revision 7

--- js/home_script.js
+++ js/home_script.js
@@ -1,6 +1,9 @@
 /* Main JS File */
 
 API_KEY = '8c8b39627a63612c01f3fed9ef6f1db7';
+var new_movies = new Array();
+start = 5;
+end = 10;
 
 
 
@@ -8,6 +11,7 @@ function getMoviesNowPlaying(){
 
 	url = 'http://api.themoviedb.org/3/movie/now_playing?api_key=' + API_KEY;
 	base_img_link = 'http://image.tmdb.org/t/p/w500';
+	counter = 0;
 
 	//Use getJSON() or ajax(), ajax is used for more control for instance
 	//failure cases, where getJSON does not acknowledge
@@ -19,13 +23,77 @@ function getMoviesNowPlaying(){
 		success: function(data){
 			
 			$(data.results).each(function(index, value){
-				$("#MoviesNowPlaying ul").append("<li>" + '<img src="' + base_img_link + value.poster_path +'">' + "</li>");
+
+				new_movies.push('<li><img src="' + base_img_link + value.poster_path +'"' + ' title="' + value.original_title + '"/><br>' + value.original_title + '</li>');
+				
+				if(counter < 5){
+					$("#MoviesNowPlaying .container ul").append('<li><img src="' + base_img_link + value.poster_path +'"' + ' title="' + value.original_title + '"/><br>' + value.original_title + '</li>');
+					counter++;
+				}
 			});
 
 		}
 	});
+}
+
+
+
+
+function prevNewMovies(){
+
+
+	if(start <= 4){
+		$(".previous").css('visibility', 'hidden');
+		$(".next").css('visibility', 'visible');
+	}
+	else if(start + 5 >= new_movies.length){
+		$(".previous").css('visibility', 'visible');
+		$(".next").css('visibility', 'hidden');
+	}
+	else{
+		$(".previous").css('visibility', 'visible');
+		$(".next").css('visibility', 'visible');
+	}
+
+	start -= 5;
+
+	end -= 5;
+
+	$("#MoviesNowPlaying .container ul").empty();
+
+	for(i = start; i < end; i++){
+		$("#MoviesNowPlaying .container ul").append(new_movies[i]);
+	}
 
 }
+
+function nextNewMovies(){
+
+
+	if(start + 5 >= new_movies.length){
+		$(".next").css('visibility', 'hidden');
+		$(".previous").css('visibility', 'visible');
+	}
+	else if(start <= 4){
+		$(".next").css('visibility', 'visible');
+		$(".previous").css('visibility', 'hidden');
+	}
+	else{
+		$(".previous").css('visibility', 'visible');
+		$(".next").css('visibility', 'visible');
+	}
+
+	$("#MoviesNowPlaying .container ul").empty();
+
+	for(i = start; i < end; i++){
+		$("#MoviesNowPlaying .container ul").append(new_movies[i]);
+	}
+	start += 5;
+
+	end += 5;
+
+}
+
 
 $(document).ready(function(){
 	getMoviesNowPlaying();
